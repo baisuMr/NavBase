b899444440a402524bb3f23fe6783617077c2a9d
BaiSu <1099116749@qq.com>
2026-09-18 15:41:33 +0800

    chore: 本地化字体与图标移除外部 CDN 依赖

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import './styles/fonts.css'
 import './styles/global.css'
 
 const app = createApp(App)
