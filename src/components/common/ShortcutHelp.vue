2c756cff894018277d1c8d4f291529a8e0effbe7
BaiSu <1099116749@qq.com>
2026-09-24 17:05:11 +0800

    chore: 精简快捷添加按钮文案并更新待办

--- src/components/common/ShortcutHelp.vue
+++ src/components/common/ShortcutHelp.vue
@@ -45,7 +45,7 @@
           @click.prevent
         >
           <i class="ri-bookmark-add-line"></i>
-          <span>快捷添加书签</span>
+          <span>快捷添加</span>
         </a>
         <p class="shortcut-bookmarklet-hint">仅桌面浏览器可用（手机浏览器无书签栏）。</p>
       </div>
